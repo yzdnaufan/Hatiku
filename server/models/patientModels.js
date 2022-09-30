e2cff5a8a320f2e1f527d8d45e035b854813bbbf
yzdnaufan <ahyaznau@gmail.com>
2022-09-30 11:07:37 +0700

    add patient model

---
+++ server/models/patientModels.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+ 
+const patient = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    age:{
+        type: Number,
+        required: true
+    },
+    height:{
+        type: Number,
+        required: true
+    },
+    weight:{
+        type: Number,
+        required: true
+    },
+    gender:{
+        type: Boolean,
+        required: true
+    },
+    ap_hi:{
+        type: Number,
+        required: true
+    },
+    ap_lo:{
+        type: Number,
+        required: true
+    },
+    cholestrol:{
+        type: Number,
+        //1: normal, 2: above normal, 3: well above normal
+        enum: [1,2,3],
+
+        required: true
+    },
+    gluc:{
+        type: Number,
+        //1: normal, 2: above normal, 3: well above normal
+        enum: [1,2,3],
+
+        required: true
+    },
+    smoke:{
+        type: Boolean,
+        required: true
+    },
+    alco:{
+        type: Boolean,
+        required: true
+    },
+    active:{
+        type: Boolean,
+        required: true
+    },
+    cardio:{
+        type: Boolean,
+        required: true
+    }
+});
+ 
+export default mongoose.model('Patients', patient);
