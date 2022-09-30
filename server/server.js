7325a97f6631114ca1ff3551be222e127ff40a46
yzdnaufan <ahyaznau@gmail.com>
2022-09-30 09:15:23 +0700

    feat: basic model and controllers

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
-const express = require("express");
+import express from "express";
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+
+import mongoose from "mongoose";
+import cors from "cors";
+import {} from "dotenv/config";
+import userRoutes from "./routes/records.js"
+
+const dbo = mongoose.connection;
+// get driver connection
+mongoose.connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+dbo.on('error', (error)=> console.error('Error: '+error));
+dbo.once('open', () => console.log('Database connected...'));
+
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
+app.use(userRoutes);
 
 
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+app.listen(port, () => console.log('Server is up and running in port: '+port));
