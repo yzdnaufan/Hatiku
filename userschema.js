ac0bdbbd9aba38738b16c8ab8d2292023239ba20
yzdnaufan <ahyaznau@gmail.com>
2022-09-30 10:55:11 +0700

    restructure project

--- userschema.js
+++
@@ -1,9 +0,0 @@
-var mongoose=require('mongoose');
- 
-var UserSchema = new mongoose.Schema({
-    UserId:Number,
-    Name:String
-});
- 
-module.exports = mongoose.model(
-    'user', UserSchema, 'Users');
