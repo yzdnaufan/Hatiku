5059db562d3aa8ee1d67817897952de664c655cb
RyanKusnadi <ryankusnadi2002@mail.ugm.ac.id>
2022-11-08 14:47:31 +0700

    Restructuring Project + Port from Flask to FastAPI
    This commit restructures the project directory. Webserver source codes are separated into 2 server folders (server-crud and server-prediction.) Furthermore, the machine learning model are now deployed using FastAPI instead of Flask. We switch to FastAPI because it is faster in performance and supports asynchronous processing.

---
+++ apps/server-diagnosis/src/main.py
@@ -0,0 +1,26 @@
+# Import libraries
+import pickle
+from typing import Union
+from fastapi import FastAPI
+import numpy as np
+
+app = FastAPI()
+
+# Load the model
+DTC_model = pickle.load(open('apps\server-diagnosis\models\DTC_model.pkl','rb'))
+KNC_model = pickle.load(open('apps\server-diagnosis\models\KNC_model.pkl','rb'))
+RFC_model = pickle.load(open('apps\server-diagnosis\models\RFC_model.pkl','rb'))
+SGD_model = pickle.load(open('apps\server-diagnosis\models\SGD_model.pkl','rb'))
+models = [DTC_model, KNC_model, RFC_model, SGD_model]
+
+@app.post("/")
+async def root(age:int,gender:int,height:float,weight:float,ap_hi:int,ap_lo:int,cholesterol:int,gluc:int,smoke:int,alco:int,active:int):
+    vote = 0
+    number_of_model = len(models)
+    for model in models:
+        prediction = model.predict(np.array([age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active]).reshape(1,-1))
+        print(prediction)
+        if prediction == [1]:
+            vote += 1 
+    
+    return {"number_of_positive": vote, "number_of_model":number_of_model}
