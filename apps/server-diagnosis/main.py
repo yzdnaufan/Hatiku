cce5936638549a08a8ac387e93380aaea32c62bb
RyanKusnadi <ryankusnadi2002@mail.ugm.ac.id>
2022-11-15 08:12:31 +0700

    Project Restructure

--- apps/server-diagnosis/main.py
+++ apps/server-diagnosis/main.py
@@ -1,3 +1,5 @@
+#uvicorn apps.server-diagnosis.src.main:app --reload
+
 # Import libraries
 import pickle
 from typing import Union
@@ -13,14 +15,17 @@ RFC_model = pickle.load(open('apps\server-diagnosis\models\RFC_model.pkl','rb'))
 SGD_model = pickle.load(open('apps\server-diagnosis\models\SGD_model.pkl','rb'))
 models = [DTC_model, KNC_model, RFC_model, SGD_model]
 
-@app.post("/")
+@app.post("/risk-evaluation/")
 async def root(age:int,gender:int,height:float,weight:float,ap_hi:int,ap_lo:int,cholesterol:int,gluc:int,smoke:int,alco:int,active:int):
+    prediction = predict_risk(age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active)
+    return {"number_of_positive": prediction["number_of_positive"], "number_of_model": prediction["number_of_model"]}
+
+
+def predict_risk(age:int,gender:int,height:float,weight:float,ap_hi:int,ap_lo:int,cholesterol:int,gluc:int,smoke:int,alco:int,active:int):
     vote = 0
     number_of_model = len(models)
     for model in models:
         prediction = model.predict(np.array([age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active]).reshape(1,-1))
-        print(prediction)
         if prediction == [1]:
-            vote += 1 
-    
+            vote += 1
     return {"number_of_positive": vote, "number_of_model":number_of_model}
