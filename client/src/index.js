288ececf4d0544665ca09e931a2950c9451e1fab
yzdnaufan <ahyaznau@gmail.com>
2022-12-04 13:45:08 +0700

    modified:   client/src/index.js     feat : add syncfusion license     feat : add navbar     feat : restructure project to component based     feat : add tailwind directive     feat : edit npm run script     feat : del redundant files     feat : add assets

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./assets/styles/index.css"
+import ReactDOM from "react-dom";
+import App from "./App";
+import {ContextProvider} from "./contexts/ContextProvider";
+import {registerLicense} from '@syncfusion/ej2-base'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerLicense("ORg4AjUWIQA/Gnt2VVhjQlFac1ZJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0RgWX1ccnJXT2BUUUM=");
+
+
+
+ReactDOM.render(
+        <ContextProvider>
+            <App/>
+        </ContextProvider>
+    ,
+    document.getElementById("root")
+)
